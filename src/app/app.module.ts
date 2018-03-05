da1c82cbfd335a97332a88dbb6a28cf794ec1797
Swaroop <swaroop1011@gmail.com>
2018-03-06 01:26:30 +0530

    adding the Incident Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IncidentComponent } from './incident/incident.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncidentComponent
   ],
   imports: [
     BrowserModule
